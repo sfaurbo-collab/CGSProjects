70ff297310ffe624be9d97f5fcfb021a92d92a93
sfaurbo-collab <sfaurbo@gmail.com>
2025-08-29 15:51:52 -0600

    Update Week4PracticalTask2.cpp
    Finished Task 2

--- Week4/Week4PracticalTask2/Week4PracticalTask2/Week4PracticalTask2.cpp
+++ Week4/Week4PracticalTask2/Week4PracticalTask2/Week4PracticalTask2.cpp
@@ -7,3 +7,33 @@
 //		- “Hey you weren’t supposed to enter 5!” when the number 5 is entered.
 //		- “Wow, you’re more patient than I am, you win.” if the user doesn’t enter 5 after 10 iterations.
 // Hint : Use the break keyword to exit the loop early.
+
+using namespace std;
+
+int main()
+{
+
+	int number = 0;
+	int counter = 0;
+
+	while (number != 5)
+	{
+		cout << "Please enter any number, except for 5: ";
+		cin >> number;
+		
+		if (counter < 9)
+		{
+			counter++;
+			cout << "Attempt: " << counter << endl;
+		}
+		else if (counter == 9)
+		{
+			cout << "Wow, you're more patient than I am, you win." << endl;
+			break;
+		}
+		else if (number == 5)
+		{
+			cout << "Hey you weren’t supposed to enter 5!" << endl;
+		}
+	}
+}
