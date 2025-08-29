70c91b99068308586cdf6cd3c970fd4739447622
sfaurbo-collab <sfaurbo@gmail.com>
2025-08-28 19:54:56 -0600

    Update Week4PracticalTask1.cpp
    Working on practical task 1

--- Week4PracticalTask1/Week4PracticalTask1/Week4PracticalTask1.cpp
+++ Week4PracticalTask1/Week4PracticalTask1/Week4PracticalTask1.cpp
@@ -5,3 +5,28 @@
 //	- Use a while loop to print numbers from 1 to 10.
 //	- Use a do - while loop to prompt the user to input a number between 1 and 10. If the number is invalid, prompt them again.
 //	- Use a for loop to print the first 10 multiples of 3 (i.e., 3, 6, 9, ..., 30).
+
+using namespace std;
+
+int main()
+{
+	int number = 0;
+
+	while (number <= 9)
+	{
+		number++;
+		cout << number << endl;
+	}
+	
+	int input;
+
+	do
+	{
+		cout << "Please enter a number from 1 to 10: " << endl;
+		cin >> input;
+	} while (input > 10 || input < 1);
+	cout << "The number you have entered is valid" << endl;
+
+
+
+}
