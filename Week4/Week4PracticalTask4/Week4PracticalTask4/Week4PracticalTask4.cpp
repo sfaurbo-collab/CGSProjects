92446b3b1102c3dc81738fa17fc84ee8474a74fa
sfaurbo-collab <sfaurbo@gmail.com>
2025-08-31 12:15:47 -0600

    Update Week4PracticalTask4
    Completed Task 4

--- Week4/Week4PracticalTask4/Week4PracticalTask4/Week4PracticalTask4.cpp
+++ Week4/Week4PracticalTask4/Week4PracticalTask4/Week4PracticalTask4.cpp
@@ -1,7 +1,50 @@
 #include <iostream>
+#include <fstream>
 
 // Objective: Practice file I/O and character array manipulation.
 // 1. Create a program that :
 //	- Loads the provided alphabet.txt file into a char array.
 //	- Replaces each lowercase letter in the array with its uppercase equivalent.
 //	- Writes the updated array to a new file named uppercase_alphabet.txt.
+
+using namespace std;
+
+int main()
+{
+	ifstream file ("alphabet.txt");
+
+	file.seekg(0, std::ios::end); 
+	long fileSize = file.tellg();
+	file.seekg(0, std::ios::beg);
+
+	char* alphabet = new char[fileSize + 1];
+
+	int i = 0;
+	char ch;
+	while (file.get(ch) && i < 52) 
+	{ 
+		if (islower(ch))
+		{
+			alphabet[i] = ch -32;
+			i++;
+		}
+		else
+		{
+			alphabet[i] = ch;
+			i++;
+		}
+	}
+	alphabet[i] = '\0';
+
+	file.close();
+
+	ofstream upperfile("UpperAlphabet.txt");
+
+	upperfile << alphabet[52] << "\n";
+
+	upperfile.write(alphabet, 52);
+
+	upperfile.close();
+
+	cout << alphabet << endl;
+}
