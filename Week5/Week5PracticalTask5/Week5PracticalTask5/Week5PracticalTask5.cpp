7d03b6c15c425e33bfc01e1e53a9355c087d3c1d
sfaurbo-collab <sfaurbo@gmail.com>
2025-09-07 15:43:17 -0600

    Week5PracticalTask5
    Worked on practice task 5

--- Week5/Week5PracticalTask5/Week5PracticalTask5/Week5PracticalTask5.cpp
+++ Week5/Week5PracticalTask5/Week5PracticalTask5/Week5PracticalTask5.cpp
@@ -1,5 +1,8 @@
+#include <fstream>
 #include <iostream>
 #include <list>
+#include <string>
+#include <vector>
 
 // Objective: Solve a specific anagram using the provided dictionary file.
 // Load the Dictionary :
@@ -10,3 +13,32 @@
 //	 - Write a program to identify all valid words in the dictionary that match this anagram.
 // Output the Results :
 //	 - Print all matching anagrams to the console.
+
+int main()
+{
+	std::list<std::string> dictionary;
+	std::ifstream textFile("dictionary.txt");
+
+	std::string line;
+	while (std::getline(textFile, line))
+	{
+		dictionary.push_back(line);
+	}
+
+	//for (std::list<std::string>::iterator it = dictionary.begin(); it != dictionary.end(); ++it) 
+	//{
+	//	std::cout << *it << std::endl;
+	//}
+
+	textFile.close();
+		 
+	std::vector<char> anagram = { 'g', 'i', 'm', 'n', 'o', 'p', 'r', 'g', 'a', 'm', 'r' };
+
+	//for (int i = 0; i < anagram.size(); i++)
+	//{
+	//	std::cout << anagram[i] << std::endl;
+	//}
+
+	//Now I have to find the words in "dictionary" with starting letters corresponding to "anagram"
+	//Then I have to print out the results
+}
