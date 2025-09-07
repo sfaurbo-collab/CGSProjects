ecbcdd7f4997699bbd925363b2d3450930880232
sfaurbo-collab <sfaurbo@gmail.com>
2025-09-07 16:08:18 -0600

    Update Week5PracticalTask4.cpp
    Working on practice task 4

--- Week5/Week5PracticalTask4/Week5PracticalTask4/Week5PracticalTask4.cpp
+++ Week5/Week5PracticalTask4/Week5PracticalTask4/Week5PracticalTask4.cpp
@@ -4,10 +4,24 @@
 // Objective: Practice unique operations of std::list.
 // Write a program that :
 //	 - Initializes a std::list<int> with at least 10 random integers.
-//	 - Uses member functions to : some text
+//	 - Uses member functions to:some text
 //		 - Add an element to the front and back of the list.
 //		 - Remove all even numbers from the list.
 //		 - Reverse the list.
 // Print the list after each operation to the console.
 
+int main()
+{
+	//Initialize a std::list<int>
 
+	std::list<int> numbers[10];
+	
+	//Generate a random number
+	
+	srand(time(0));
+		
+	int randomNum = rand() % 101;
+	
+	//insert the random number into the list
+
+}
