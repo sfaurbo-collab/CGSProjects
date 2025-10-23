eb0d620c4aee8e6c67975a51cb82dbd62ea44ec8
sfaurbo-collab <sfaurbo@gmail.com>
2025-10-22 20:05:07 -0600

    Update MainGame.cpp

--- PongRedux/HelloWorld/MainGame.cpp
+++ PongRedux/HelloWorld/MainGame.cpp
@@ -6,7 +6,7 @@ int DISPLAY_WIDTH = 1280;
 int DISPLAY_HEIGHT = 720;
 int DISPLAY_SCALE = 1;
 
-enum Agent8State
+enum Agent8State //StateOfGame. --> look at this to reset when a score is made
 {
 	STATE_APPEAR = 0,
 	STATE_HALT,
@@ -40,12 +40,9 @@ enum GameObjectType
 
 void HandlePlayerControls();
 void UpdateBall();
-//void UpdateFan();
 void UpdateTools();
 void UpdateCoinsandStars();
-//void UpdateLasers();
-//void UpdateDestroyed();
-//void UpdateAgent8();
+void UpdateAgent8();
 
 // The entry point for a PlayBuffer program
 void MainGameEntry( PLAY_IGNORE_COMMAND_LINE )
@@ -54,29 +51,22 @@ void MainGameEntry( PLAY_IGNORE_COMMAND_LINE )
 	Play::CentreAllSpriteOrigins();
 	Play::LoadBackground("Data\\Backgrounds\\background.png");
 	Play::StartAudioLoop("music");
-	//Play::CreateGameObject(TYPE_AGENT8, { 115, 600 }, 50, "agent8");
-	//int id_fan = Play::CreateGameObject(TYPE_FAN, { 1140, 503 }, 0, "fan");
-	//Play::GetGameObject(id_fan).velocity = { 0, -3 };
-	//Play::GetGameObject(id_fan).animSpeed = 1.0f;
 	Play::CreateGameObject(TYPE_PADDLEL, { 100, DISPLAY_HEIGHT / 2 }, 50, "PaddleL");
 	Play::CreateGameObject(TYPE_PADDLER, { 1180, DISPLAY_HEIGHT / 2 }, 50, "PaddleR");
 	int id_ball = Play::CreateGameObject(TYPE_BALL, { DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2 }, 50, "ball");
-	Play::GetGameObject(id_ball).velocity = { 2, 1 };
+	Play::GetGameObject(id_ball).velocity = { 1, 1 };
 }
 
 // Called by PlayBuffer every frame (60 times a second!)
 bool MainGameUpdate( float elapsedTime )
 {	
 	Play::DrawBackground();
-	//UpdateAgent8();
-	//UpdateFan();
 	HandlePlayerControls();
 	UpdateBall();
 	UpdateTools();
 	UpdateCoinsandStars();
-	//UpdateLasers();
-	//UpdateDestroyed();
-	Play::DrawFontText("32px", "LEFT SIDE: W = UP, S = DOWN. RIGHT SIDE: ARROW KEYS TO MOVE UP AND DOWN",
+	UpdateAgent8();
+	Play::DrawFontText("32px", "LEFT SIDE: W = UP, S = DOWN.                RIGHT SIDE: ARROW KEYS TO MOVE UP AND DOWN",
 		{ DISPLAY_WIDTH / 2, 40 }, Play::CENTRE);
 	Play::DrawFontText("32px", "Player 1 SCORE: " + std::to_string(gameState.scoreP1),
 		{ 150, DISPLAY_HEIGHT - 40 }, Play::CENTRE);
@@ -95,40 +85,7 @@ int MainGameExit( void )
 
 void HandlePlayerControls()
 {
-	//GameObject& obj_agent8 = Play::GetGameObjectByType(TYPE_AGENT8);
-	//if (Play::KeyDown(Play::KEY_UP))
-	//{
-	//	obj_agent8.velocity = { 0, 4 };
-	//	Play::SetSprite(obj_agent8, "agent8_climb", 0.25f);
-	//}
-	//else if (Play::KeyDown(Play::KEY_DOWN))
-	//{
-	//	obj_agent8.acceleration = { 0, -1 };
-	//	Play::SetSprite(obj_agent8, "agent8_fall", 0);
-	//}
-	//else
-	//{
-	//	if (obj_agent8.velocity.y < -5)
-	//	{
-	//		gameState.agentState = STATE_HALT;
-	//		Play::SetSprite(obj_agent8, "agent8_halt", 0.333f);
-	//		obj_agent8.acceleration = { 0, 0 };
-	//	}
-	//	else
-	//	{
-	//		Play::SetSprite(obj_agent8, "agent8_hang", 0.02f);
-	//		obj_agent8.velocity *= 0.5f;
-	//		obj_agent8.acceleration = { 0, 0 };
-	//	}
-	//}
-	//if (Play::KeyPressed(Play::KEY_SPACE))
-	//{
-	//	Vector2D firePos = obj_agent8.pos + Vector2D(155, 75);
-	//	int id = Play::CreateGameObject(TYPE_LASER, firePos, 30, "laser");
-	//	Play::GetGameObject(id).velocity = { 32, 0 };
-	//	//Play::PlayAudio("shoot");
-	//}
-	GameObject& obj_paddleL = Play::GetGameObjectByType(TYPE_PADDLEL);
+		GameObject& obj_paddleL = Play::GetGameObjectByType(TYPE_PADDLEL);
 	if (Play::KeyDown(Play::KEY_W))
 	{
 		obj_paddleL.velocity = { 0, 10 };
@@ -222,83 +179,44 @@ void UpdateBall()
 	}
 	Play::UpdateGameObject(obj_ball);
 
-	if (Play::IsLeavingDisplayArea(obj_ball))
+	if (Play::IsLeavingDisplayArea(obj_ball, Play::VERTICAL))
 	{
 		obj_ball.pos = obj_ball.oldPos;
+		obj_ball.velocity.y *= -1;
+	}
+	Play::DrawObjectRotated(obj_ball);
 
+	if (Play::IsLeavingDisplayArea(obj_ball, Play::HORIZONTAL))
+	{
+		obj_ball.pos = obj_ball.oldPos;
 		double minX = obj_ball.radius;
 		double maxX = 1280.0 - obj_ball.radius;
-		double minY = obj_ball.radius;
-		double maxY = 720.0 - obj_ball.radius;
-
-		if (obj_ball.pos.x < minX || obj_ball.pos.x > maxX)
+		
+		if (obj_ball.pos.x < minX)
 		{
-			obj_ball.velocity.x *= -1;
-			//if (obj_ball.pos.x < minX) obj_ball.pos.x = minX;
-			//if (obj_ball.pos.x > maxX) obj_ball.pos.x = maxX;
+			gameState.scoreP1 += 1;
+			obj_ball.velocity = { 0, 0 };
 		}
-
-		if (obj_ball.pos.y < minY || obj_ball.pos.y > maxY)
+		else if (obj_ball.pos.x > maxX)
 		{
-			obj_ball.velocity.y *= -1;
-			//if (obj_ball.pos.y < minY) obj_ball.pos.y = minY;
-			//if (obj_ball.pos.y > maxY) obj_ball.pos.y = maxY;
+			gameState.scoreP2 += 1;
+			obj_ball.velocity = { 0, 0 };
 		}
 	}
-	Play::DrawObject(obj_ball);
+	Play::DrawObjectRotated(obj_ball);
 
 	if (Play::IsColliding(obj_ball, obj_paddleL))
 	{
 		obj_ball.velocity = { -1, -1 };
-		//obj_ball.velocity.x *= -1;
-		//obj_ball.velocity.y *= -1;
 	}
 	Play::UpdateGameObject(obj_ball);
 
 	if (Play::IsColliding(obj_ball, obj_paddleR))
 	{
 		obj_ball.velocity = { -1, -1 };
-		//obj_ball.velocity.x *= 1;
-		//obj_ball.velocity.y *= -1;
 	}
 	Play::UpdateGameObject(obj_ball);
 }
-
-
-//void UpdateFan()
-//{
-//	GameObject& obj_fan = Play::GetGameObjectByType(TYPE_FAN);
-//	if (Play::RandomRoll(50) == 50)
-//	{
-//		int id = Play::CreateGameObject(TYPE_TOOL, obj_fan.pos, 50, "driver");
-//		GameObject& obj_tool = Play::GetGameObject(id);
-//		obj_tool.velocity = Point2f(-8, Play::RandomRollRange(-1, 1) * 6);
-//		
-//		if (Play::RandomRoll(2) == 1)
-//		{
-//			Play::SetSprite(obj_tool, "spanner", 0);
-//			obj_tool.radius = 100;
-//			obj_tool.velocity.x = -4;
-//			obj_tool.rotSpeed = 0.1f;
-//		}
-//		//Play::PlayAudio("tool");
-//	}
-//	if (Play::RandomRoll(150) == 1)
-//	{
-//		int id = Play::CreateGameObject(TYPE_COIN, obj_fan.pos, 40, "coin");
-//		GameObject& obj_coin = Play::GetGameObject(id);
-//		obj_coin.velocity = { -3, 0 };
-//		obj_coin.rotSpeed = 0.1f;
-//	}
-//	Play::UpdateGameObject(obj_fan);
-//	
-//	if (Play::IsLeavingDisplayArea(obj_fan))
-//	{
-//		obj_fan.pos = obj_fan.oldPos;
-//		obj_fan.velocity.y *= -1;
-//	}
-//	Play::DrawObject(obj_fan);
-//}
 
 void UpdateTools()
 {
@@ -376,115 +294,54 @@ void UpdateCoinsandStars()
 	}
 }
 
-//void UpdateLasers()
-//{
-//	std::vector<int> vLasers = Play::CollectGameObjectIDsByType(TYPE_LASER);
-//	std::vector<int> vTools = Play::CollectGameObjectIDsByType(TYPE_TOOL);
-//	std::vector<int> vCoins = Play::CollectGameObjectIDsByType(TYPE_COIN);
-//	
-//	for (int id_laser : vLasers)
-//	{
-//		GameObject& obj_laser = Play::GetGameObject(id_laser);
-//		bool hasCollided = false;
-//		for (int id_tool : vTools)
-//		{
-//			GameObject& obj_tool = Play::GetGameObject(id_tool);
-//			if (Play::IsColliding(obj_laser, obj_tool))
-//			{
-//				hasCollided = true;
-//				obj_tool.type = TYPE_DESTROYED;
-//				gameState.scoreP1 += 100;
-//			}
-//		}
-//		
-//		for (int id_coin : vCoins)
-//		{
-//			GameObject& obj_coin = Play::GetGameObject(id_coin);
-//			if (Play::IsColliding(obj_laser, obj_coin))
-//			{
-//				hasCollided = true;
-//				obj_coin.type = TYPE_DESTROYED;
-//				//Play::PlayAudio("error");
-//				gameState.scoreP1 -= 300;
-//			}
-//		}
-//		if (gameState.scoreP1 < 0)
-//			gameState.scoreP1 = 0;
-//		
-//		Play::UpdateGameObject(obj_laser);
-//		Play::DrawObject(obj_laser);
-//		
-//		if (!Play::IsVisible(obj_laser) || hasCollided)
-//			Play::DestroyGameObject(id_laser);
-//	}
-//}
-//
-//void UpdateDestroyed()
-//{
-//	std::vector<int> vDead = Play::CollectGameObjectIDsByType(TYPE_DESTROYED);
-//	
-//	for (int id_dead : vDead)
-//	{
-//		GameObject& obj_dead = Play::GetGameObject(id_dead);
-//		obj_dead.animSpeed = 0.2f;
-//		Play::UpdateGameObject(obj_dead);
-//		
-//		if (obj_dead.frame % 2)
-//			Play::DrawObjectRotated(obj_dead, (10 - obj_dead.frame) / 10.0f);
-//		
-//		if (!Play::IsVisible(obj_dead) || obj_dead.frame >= 10)
-//			Play::DestroyGameObject(id_dead);
-//	}
-//}
-//void UpdateAgent8()
-//{
-//	GameObject& obj_agent8 = Play::GetGameObjectByType(TYPE_AGENT8);
-//	
-//	switch (gameState.agentState)
-//	{
-//	case STATE_APPEAR:
-//		obj_agent8.velocity = { 0, -12 };
-//		obj_agent8.acceleration = { 0, -0.5f };
-//		Play::SetSprite(obj_agent8, "agent8_fall", 0);
-//		obj_agent8.rotation = 0;
-//		if (obj_agent8.pos.y <= DISPLAY_HEIGHT * 0.66f)
-//			gameState.agentState = STATE_PLAY;
-//		break;
-//	
-//	case STATE_HALT:
-//		obj_agent8.velocity *= 0.9f;
-//		if (Play::IsAnimationComplete(obj_agent8))
-//			gameState.agentState = STATE_PLAY;
-//		break;
-//	
-//	case STATE_PLAY:
-//		HandlePlayerControls();
-//		break;
-//	
-//	case STATE_DEAD:
-//		obj_agent8.acceleration = { -0.3f , 0.5f };
-//		obj_agent8.rotation += 0.25f;
-//		if (Play::KeyPressed(Play::KEY_SPACE) == true)
-//		{
-//			gameState.agentState = STATE_APPEAR;
-//			obj_agent8.pos = { 115, 600 };
-//			obj_agent8.velocity = { 0, 0 };
-//			obj_agent8.frame = 0;
-//			Play::StartAudioLoop("music");
-//			gameState.scoreP1 = 0;
-//			
-//			for (int id_obj : Play::CollectGameObjectIDsByType(TYPE_TOOL))
-//				Play::GetGameObject(id_obj).type = TYPE_DESTROYED;
-//		}
-//		break;
-//	
-//	} // End of switch on Agent8State
-//	
-//	Play::UpdateGameObject(obj_agent8);
-//	
-//	if (Play::IsLeavingDisplayArea(obj_agent8) && gameState.agentState != STATE_DEAD)
-//		obj_agent8.pos = obj_agent8.oldPos;
-//	
-//	Play::DrawLine({ obj_agent8.pos.x, 720 }, obj_agent8.pos, Play::cWhite);
-//	Play::DrawObjectRotated(obj_agent8);
-//}
+void UpdateAgent8()
+{
+	GameObject& obj_agent8 = Play::GetGameObjectByType(TYPE_AGENT8);
+
+	switch (gameState.agentState)
+	{
+	case STATE_APPEAR:
+		obj_agent8.velocity = { 0, -12 };
+		obj_agent8.acceleration = { 0, -0.5f };
+		Play::SetSprite(obj_agent8, "agent8_fall", 0);
+		obj_agent8.rotation = 0;
+		if (obj_agent8.pos.y <= DISPLAY_HEIGHT * 0.66f)
+			gameState.agentState = STATE_PLAY;
+		break;
+
+	case STATE_HALT:
+		obj_agent8.velocity *= 0.9f;
+		if (Play::IsAnimationComplete(obj_agent8))
+			gameState.agentState = STATE_PLAY;
+		break;
+
+	case STATE_PLAY:
+		HandlePlayerControls();
+		break;
+
+	case STATE_DEAD:
+		obj_agent8.acceleration = { -0.3f, 0.5f };
+		obj_agent8.rotation += 0.25f;
+		if (Play::KeyPressed(Play::KEY_SPACE) == true)
+		{
+			gameState.agentState = STATE_APPEAR;
+			obj_agent8.pos = { 115, 600 };
+			obj_agent8.velocity = { 0, 0 };
+			obj_agent8.frame = 0;
+			Play::StartAudioLoop("music");
+			//gameState.score = 0;
+
+			for (int id_obj : Play::CollectGameObjectIDsByType(TYPE_TOOL))
+				Play::GetGameObject(id_obj).type = TYPE_DESTROYED;
+		}
+		break;
+	}
+
+	Play::UpdateGameObject(obj_agent8);
+
+	if (Play::IsLeavingDisplayArea(obj_agent8) && gameState.agentState != STATE_DEAD)
+		obj_agent8.pos = obj_agent8.oldPos;
+
+	Play::DrawLine({ obj_agent8.pos.x, 720 }, obj_agent8.pos, Play::cWhite);
+	Play::DrawObjectRotated(obj_agent8);
+}
